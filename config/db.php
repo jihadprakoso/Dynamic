661afc6e1def7b351146f5ae691c8d111264088d
jihadprakoso <jihadprakoso@gmail.com>
2026-06-13 17:04:17 +0700

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,23 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "db_stockbarang";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            // Display clean error or log it
+            die("Database Connection Failed: " . $exception->getMessage());
+        }
+        return $this->conn;
+    }
+}
+?>
