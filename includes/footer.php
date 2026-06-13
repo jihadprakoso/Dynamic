b1dd6f6e573309cf7993e048ababc6b7e0096fd5
jihadprakoso <jihadprakoso@gmail.com>
2026-06-13 17:04:14 +0700

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+    </div> <!-- Closes .main-content -->
+</div> <!-- Closes .d-flex -->
+
+<!-- Bootstrap 5 JS Bundle -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/bundle/min.js"></script>
+</body>
+</html>
